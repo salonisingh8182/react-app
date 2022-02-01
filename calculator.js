ee84d1d0a1d3a7ba9abc5fd847a43246141a756b
salonisingh8182 <85901903+salonisingh8182@users.noreply.github.com>
2022-02-01 14:11:29 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/bmicalculator",function(req,res){
+    res.sendFile(__dirname+"/bmicalculator.html");
+});
+
+app.post("/bmicalculator",function(req,res){
+
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var result = weight/(height*height);
+    res.send("The result is :" +result);
+});
+
+app.listen(3000,function(){
+    console.log("server started")
+});
